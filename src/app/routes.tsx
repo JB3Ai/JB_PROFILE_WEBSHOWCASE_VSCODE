9ca4c350c36020e5b1fb486d232ca35b1d27169a
JB3Ai <jono@jonoblackburn.com>
2026-07-11 17:19:35 +0200

    Add wildcard 404 fallback route and NotFound page

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -3,12 +3,14 @@ import PublicHome from '../pages/PublicHome'
 import Login from '../pages/Login'
 import RequestAccess from '../pages/RequestAccess'
 import RestrictedOS from '../pages/RestrictedOS'
+import NotFound from '../pages/NotFound'
 
 const routes = [
   { path: '/', element: <PublicHome /> },
   { path: '/login', element: <Login /> },
   { path: '/request-access', element: <RequestAccess /> },
-  { path: '/os', element: <RestrictedOS /> }
+  { path: '/os', element: <RestrictedOS /> },
+  { path: '*', element: <NotFound /> }
 ]
 
 export default routes
